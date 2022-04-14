2ac8cc0ac4e47a3b75cb8cb97ec269e5b3dbd022
01chrystian <99342538+01chrystian@users.noreply.github.com>
2022-04-13 21:57:52 -0300

    Update troco.java

--- troco.java
+++ troco.java
@@ -11,26 +11,26 @@ public class troco {
 		DecimalFormat formatador = new DecimalFormat("###,##0.00");
 		
 		int nota[] = {100, 50, 20, 10, 5, 2, 1};
-	    int centavos[] = {50, 25, 10, 5, 1};
+	        int centavos[] = {50, 25, 10, 5, 1};
 		int i, valor, conta;
 		
 		System.out.println("Digite o valor do produto: ");
 		double x = sc.nextDouble();
-		System.out.println("Digite o valor � pagar: ");
+		System.out.println("Digite o valor a pagar: ");
 		double y= sc.nextDouble();
 		
 		double calculos = calculosTroco(x,y);
 	    
-	    if (x > y) {
-	    	System.out.println("Vo�� deve: "+"R$ "+ calculos );
-	    }
-	    if (x == y){
+	        if (x > y) {
+	    	System.out.println("Voce deve: "+"R$ "+ calculos );
+	        }
+	        if (x == y){
 	    	System.out.println("Valor debitado");
-	    }
+	        }
 		if (x < y) {
 			System.out.println("Seu troco: "+"R$ "+ calculos * (-1) +"\n" );
 		
-        valor = (int) calculos;
+              valor = (int) calculos;
 		i = 0;
 	      while (valor != 0) {
 	        conta = valor / nota[i]; 
